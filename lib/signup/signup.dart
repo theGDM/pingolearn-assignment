b1a395bc1f0cfb6a5a20503110362fd05e1039dd
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2024-07-30 15:22:46 +0530

    Gyandeep | Created UI for the signup screen

--- lib/signup/signup.dart
+++ lib/signup/signup.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce/common/widgets/common_helper_widget.dart';
+import 'package:ecommerce/login/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -17,14 +19,47 @@ class _SignupState extends State<Signup> {
         systemOverlayStyle: Theme.of(context).appBarTheme.systemOverlayStyle!.copyWith(
               statusBarColor: Theme.of(context).colorScheme.primary,
               statusBarIconBrightness: Brightness.dark,
+              systemNavigationBarColor: Theme.of(context).colorScheme.primary,
             ),
       ),
       body: Container(
-        margin: EdgeInsets.only(top: 16.w, left: 16.w, right: 16.w, bottom: 10.h),
+        margin: EdgeInsets.only(top: 16.w, left: 16.w, right: 16.w, bottom: 16.h),
         width: MediaQuery.of(context).size.width,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                buildHeadingText(context),
+              ],
+            ),
+            SizedBox(
+              child: Column(
+                children: [
+                  buildTextField(context, 'Name', false, (name) {}),
+                  buildTextField(context, 'Email', false, (email) {}),
+                  buildTextField(context, 'Password', true, (passowrd) {}),
+                ],
+              ),
+            ),
+            SizedBox(
+              child: Column(
+                children: [
+                  buildSubmitButton(context, 'Signup', false, () {}),
+                  SizedBox(height: 5.h),
+                  buildBottomNavigation(context, "Already have an account? ", 'Login', () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => const Login(),
+                      ),
+                    );
+                  }),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
