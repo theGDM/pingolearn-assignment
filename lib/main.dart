c6537b7cfeb734fea845aaa190b572e48f7168cd
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2024-07-30 13:15:57 +0530

    Gyandeep | Added project to the firebase using firebase cli

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:ecommerce/dashboard/dashboard.dart';
 import 'package:ecommerce/login/login.dart';
 import 'package:ecommerce/theme.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -17,7 +20,7 @@ class MyApp extends StatelessWidget {
     User? user = FirebaseAuth.instance.currentUser;
     return ScreenUtilInit(
       builder: (context, child) => MaterialApp(
-        title: 'Droame Event App',
+        title: 'Pingolearn Ecommerce Website',
         debugShowCheckedModeBanner: false,
         theme: appTheme,
         home: user == null ? const Login() : const Dashboard(),
